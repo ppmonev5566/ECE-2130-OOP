b563f19e6c40f52e5c7f771c46c5c67880d60106
ppmonev5566 <ppmonev@gmail.com>
2025-08-22 15:20:56 -0700

    Create Hello.cpp
    Added simple hello world program to showcase git functionality

---
+++ Lab1/Hello.cpp
@@ -0,0 +1,10 @@
+//simple hello world program to showcase functionality
+
+#include <iostream>
+
+using namespace std;
+int main() {
+    cout << "Hello, World!" << endl;
+    printf("Hello again!\n");
+    return 0;
+}
